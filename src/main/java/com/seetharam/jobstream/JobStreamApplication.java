1576870e3d6861ad9101e0f3c29acea1acaab6f7
Seetha Ram Rao Vanam <seetharam2602@gmail.com>
2026-02-10 20:06:11 -0600

    Project Initial Commit

---
+++ src/main/java/com/seetharam/jobstream/JobStreamApplication.java
@@ -0,0 +1,13 @@
+package com.seetharam.jobstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobStreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seetharam/jobstream/JobStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seetharam.jobstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
